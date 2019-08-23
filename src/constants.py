8ff47c83254b09c93cd54f7dba075f161e06a91b
Winfred Villaluna <winfred_villaluna@dlsu.edu.ph>
2019-08-23 13:49:49 +0800

    Adds basic database connection implementations

---
+++ src/constants.py
@@ -0,0 +1,25 @@
+LOCATION = "localhost"
+USERNAME = "root"
+PASSWORD = "password"
+SCHEMA = "orsen_kb"
+
+####################################################
+# RELATIONS ########################################
+####################################################
+IS_A = "IsA"
+PART_OF = "PartOf"
+AT_LOCATION = "AtLocation"
+HAS_PREREQ = "HasPrerequisite"
+CREATED_BY = "CreatedBy"
+USED_FOR = "UsedFor"
+CAUSES = "Causes"
+DESIRES = "Desires"
+CAPABLE_OF = "CapableOf"
+HAS_PROPERTY = "HasProperty"
+HAS_A = "HasA"
+RECEIVES_ACTION = "ReceivesAction"
+
+LOCATED_NEAR = "LocatedNear"
+
+RELATIONS = [IS_A, PART_OF, AT_LOCATION, HAS_PREREQ, CREATED_BY, USED_FOR, CAUSES, DESIRES, CAPABLE_OF, HAS_PROPERTY,
+             HAS_A, RECEIVES_ACTION]
---
+++ src/db/SQLConnector.py
@@ -0,0 +1,31 @@
+from src.constants import *
+import pymysql
+import sys
+
+class SQLConnector:
+
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if SQLConnector.__instance == None:
+            SQLConnector()
+        return SQLConnector.__instance
+
+    def __init__(self):
+        """ Virtually private constructor. """
+        if SQLConnector.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            SQLConnector.__instance = self
+
+    def get_connection(self):
+        assert LOCATION != "", "MySQL location is not set. Set them via set_connection_details"
+        assert USERNAME != "", "MySQL username is not set. Set them via set_connection_details"
+        assert PASSWORD != "", "MySQL password is not set. Set them via set_connection_details"
+
+        try:
+            return pymysql.connect(LOCATION, USERNAME, PASSWORD, SCHEMA)
+        except Exception as e:
+            print(e, file=sys.stderr)
+
