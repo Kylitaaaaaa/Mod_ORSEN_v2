359794dbf871844941f8b6ad8931ce7321fc2000
Kylitaaaaaa <kylealtheaanne@gmail.com>
2019-09-02 17:52:20 +0800

    Dialogue Planner

--- src/DialoguePlanner.py
+++
@@ -1,9 +0,0 @@
-
-
-class DialoguePlanner:
-    def __init__(self):
-        self.dialogue_moves = []
-
-    def add_dialogue_move(self, dialogue_move):
-        self.dialogue_moves.add(dialogue_move)
-
