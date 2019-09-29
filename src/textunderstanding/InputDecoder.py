fb4b9749ec416ea67553171c3eda6e3649bba4a3
Kyle <kylealtheaanne@gmail.com>
2019-09-29 15:00:50 +0800

    Working Input decoder?

--- src/textunderstanding/InputDecoder.py
+++ src/textunderstanding/InputDecoder.py
@@ -1,6 +1,7 @@
 from src.dataprocessor import Annotator
 from src.dataprocessor.coreference import SpacyCoreference
 
+
 import spacy
 nlp = spacy.load('en_core_web_sm')
 
@@ -22,39 +23,48 @@ class InputDecoder:
             self.annotator = Annotator()
 
     def perform_input_decoding(self, text):
+        print("TESTING")
+
         print("---------Perform Input Decoding---------")
-        #categorization
+        #categorization ###probs add intents here TODO
 
 
         #coreference resolving thru spacy
+        print("*Starting Coreferencing*")
         # Winfred's code
         referenced_text = InputDecoder.coref_resolve(self, text)
-        print("Coreferenced Text: ", referenced_text)
+        print(referenced_text)
+        print("*Done Coreferencing* \n")
 
         #dependency parsing thru spacy
         # https://spacy.io/usage/linguistic-features
+        print("*Starting Dependency Parsing*")
         parsed_doc = nlp(referenced_text)
-        print("Parsed: ", parsed_doc)
+        print(parsed_doc)
+        print("*Done Dependency Parsing* \n")
 
         #named entity recognition
-        print("Named Entities")
-        # for X in parsed_doc.ents:
+        print("*Starting Entity Recognition*")
+        named_entities = InputDecoder.get_named_entities(self, parsed_doc)
+        print(named_entities)
+        print("*Done With Entity Recognition* \n")
 
-        print([(X.text, X.label_) for X in parsed_doc.ents])
         print("---------Done with Input Decoding---------")
 
-    def get_named_entities(self, dec):
-        # named_entities = []
-        # for X in doc.ents:
-        return None
+    def get_named_entities(self, doc):
+        named_entities = []
+        for X in doc.ents:
+            named_entities.append(X)
+            # print(X.text, X.label_)
+            # named_entities.push(X.text, X.label_)
+
+        return named_entities
 
 
     def coref_resolve(self, raw_text):
         self.annotator.annotate(raw_text)
         crf = SpacyCoreference(self.annotator)
         resolved = crf.resolve()
-        print("Printing resolved text")
-        print(resolved.text)
         return resolved._.coref_resolved
 
 
