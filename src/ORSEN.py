19c84711b23c1e92c8a0a6de1e3fc3f1b49fa168
Kyle <kylealtheaanne@gmail.com>
2019-12-22 17:53:36 +0800

    Cleaning code

--- src/ORSEN.py
+++ src/ORSEN.py
@@ -74,12 +74,12 @@ class ORSEN:
         if response != "":
             Logger.log_event_response_eval(response)
 
-        orsen_reply = self.perform_dialogue_manager(response, preselected_move=move_to_execute)
-        # try:
-        #     orsen_reply = self.perform_dialogue_manager(response, preselected_move=move_to_execute)
-        # except Exception as e:
-        #     Logger.log_conversation("ERROR: " + str(e))
-        #     orsen_reply = "I see. What else can you say about that?"
+        # orsen_reply = self.perform_dialogue_manager(response, preselected_move=move_to_execute)
+        try:
+            orsen_reply = self.perform_dialogue_manager(response, preselected_move=move_to_execute)
+        except Exception as e:
+            Logger.log_conversation("ERROR: " + str(e))
+            orsen_reply = "I see. What else can you say about that?"
 
         Logger.log_conversation("ORSEN LATENCY TIME (seconds): " + str(time.time() - start_time))
 
